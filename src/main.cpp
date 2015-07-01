13ac577acff0c8808d2d633d7136464c4f2edb0c
Abhijeet Nayak <abnayak@outlook.com>
2015-06-30 23:59:41 -0700

    Redesigned backup main window

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Set the style sheet of the group box
+    //a.setStyleSheet("QGroupBox { border: 1px solid gray; border-radius: 9px; margin-top: 0.5em;}");
+    //a.setStyleSheet("QGroupBox::title { subcontrol-origin: margin;left: 10px; padding: 0 3px 0 3px;}");
+
     MainWindow w;
     w.show();
 
